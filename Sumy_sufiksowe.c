5046927fd1726ba77bc07de82fa9a303e5153e1a
Bartek <bartek9393@poczta.onet.pl>
2017-05-05 18:35:08 +0100

    Przeniesiono definicje zmiennych poza pętle

--- Sumy_sufiksowe.c
+++ Sumy_sufiksowe.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    int numbersCount;
+    int numbersCount, i, j;
     scanf("%d", &numbersCount);
     if (numbersCount < 1 || numbersCount > 50000) {
         return 1;
     }
     int array[numbersCount];
-    for (int i = 0; i < numbersCount; ++i) {
+    for (i = 0; i < numbersCount; ++i) {
         scanf("%d", &array[i]);
         if (array[i] < 1 || array[i] > 20000) {
             return 1;
         }
     }
-    for (int i = 0; i < numbersCount; ++i) {
+    for (i = 0; i < numbersCount; ++i) {
         int sum = 0;
-        for (int j = i; j < numbersCount; ++j) {
+        for (j = i; j < numbersCount; ++j) {
             sum += array[j];
         }
         printf("%d ", sum);
