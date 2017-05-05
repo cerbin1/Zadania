670b292b0b0eda7fcf83d61dc26e67d4b7d8b9e7
Bartek <bartek9393@poczta.onet.pl>
2017-05-05 21:46:17 +0100

    Dodano zadanie "Maly symbol Newtona"

---
+++ MalySymbolNewtona.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int countFactorial(int number) {
+    if (number < 2) {
+        return 1;
+    }
+    return number * countFactorial(number - 1);
+}
+
+int main() {
+    int n = 10, k = 1;
+    scanf("%d", &n);
+    scanf("%d", &k);
+    if (k < 0 || k > 10) {
+        return 1;
+    }
+    if (n < k || n > 10) {
+        return 1;
+    }
+
+    printf("%d", countFactorial(n) / (countFactorial(k) * countFactorial(n - k)));
+
+    return 0;
+}
