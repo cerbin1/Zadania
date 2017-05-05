b7a53c7ce2bc7d638f5163ef33155fd7e93ec610
Bartek <bartek9393@poczta.onet.pl>
2017-05-05 23:39:07 +0100

    Przeniesiono definicje zmiennych poza pętle

--- klasy.c
+++ klasy.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    int n, index = -1;
+    int n, index = -1, i;
     scanf("%d", &n);
     if (n < 0 || n > 1000) {
         return 1;
     }
     int array[n];
-    for (int i = 0; i < n; ++i) {
+    for (i = 0; i < n; ++i) {
         scanf("%d", &array[i]);
         if (array[i] == 0) index = i;
     }
